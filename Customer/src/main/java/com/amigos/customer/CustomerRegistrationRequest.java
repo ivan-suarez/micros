e323e176e2087b470aa22f2c25b70a25e45c1bb5
ivan-suarez <ivan.suarez-castineiras@capgemini.com>
2022-03-17 17:34:24 +0100

    first commit

---
+++ Customer/src/main/java/com/amigos/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,5 @@
+package com.amigos.customer;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+
+}
---
+++ Customer/src/main/java/com/amigos/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package com.amigos.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+
+	public void registerCustomer(CustomerRegistrationRequest request) {
+		// TODO Auto-generated method stub
+		Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+		customerRepository.save(customer);
+	}
+
+}
